aac5374243e5410c51a4791955e2aeb7070aa4e9
ykostiannikov <Yaroslav.Kostiannikov@lookout.com>
2018-03-31 12:41:38 +0300

    update

--- php-app/index.php
+++ php-app/index.php
@@ -1,4 +1,4 @@
-<h1>Hello World!!!</h1>
+<h1>Hello PaaS DevOps World!!!</h1>
 <h3>PHP Version <?= phpversion() ?></h3>
 <p><a href="/static.html">Static HTML Page</a></p>
 <p><a href="/mysql.php">MySQL test page</a></p>
